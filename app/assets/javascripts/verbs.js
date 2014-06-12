50cdbc83a2d91b65a932592e9bfdb2b1acdddd99
Alicja <acyganiewicz@gmail.com>
2014-06-12 10:51:06 +0200

    JS: Uncheck all [tenses] button, special chars buttons

---
+++ app/assets/javascripts/verbs.js
@@ -0,0 +1,37 @@
+window.onload = function(){
+  var uncheck_button = document.getElementById("uncheck_all");
+  if (uncheck_button) {
+    uncheck_button.addEventListener("click", function(){
+
+      var tenses = document.querySelectorAll("div.choose_tense input");
+      if(uncheck_button.getAttribute("data-check") == "1"){
+        for (var i = tenses.length - 1; i >= 0; i--) {
+          tenses[i].removeAttribute('checked')
+        };
+        uncheck_button.innerHTML = "Check all";
+        uncheck_button.setAttribute("data-check", "0");
+      }
+      else{
+        for (var i = tenses.length - 1; i >= 0; i--) {
+          tenses[i].setAttribute('checked', 'checked')
+        };
+        uncheck_button.innerHTML = "Uncheck All";
+        uncheck_button.setAttribute("data-check", "1");
+
+      }
+    }, false);
+  };
+
+  var chars = document.querySelectorAll("div#special_char button");
+  var input_ans = document.getElementById("answer");
+
+  var add_char = function(){
+    var val = this.innerHTML;
+    input_ans.value = input_ans.value + val.replace(/ /g, '');
+  }
+
+  for (var i = chars.length - 1; i >= 0; i--) {
+    chars[i].addEventListener("click", add_char, false);
+  };
+
+}
